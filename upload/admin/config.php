4459373ad2ca8b0d15bff9146d378ed7c756e2da
ahmed salah <ahmedfci2014@gmail.com>
2016-04-19 17:22:44 +0200

    1

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/openCart/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/openCart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/openCart/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/openCart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/openCart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/html/openCart/upload/system/');
+define('DIR_IMAGE', '/var/www/html/openCart/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/openCart/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/openCart/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/openCart/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/openCart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/openCart/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/openCart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/openCart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/openCart/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/openCart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'openCart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/openCart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/openCart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/openCart/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/html/openCart/upload/system/');
+define('DIR_IMAGE', '/var/www/html/openCart/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/openCart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/openCart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/openCart/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/openCart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/openCart/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/openCart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/openCart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/openCart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'openCart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
